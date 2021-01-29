b622cdbb4c6891fc18fad7927a065bc07fdcfba4
GRGRV9 <niamod@mail.ru>
2021-01-29 21:18:35 +0900

    29.01.2021 21:18

---
+++ New Unity Project/Assets/Scripts/StartManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+    public Animator ShipAnimator;
+
+    void Start()
+    {
+        ShipAnimator = GetComponent<Animator>();
+    }
+
+
+    public void ShipStartGame()
+    {
+        ShipAnimator.SetBool("IsStarted", true);
+    }
+}
